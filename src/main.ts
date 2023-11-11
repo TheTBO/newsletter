fa2ea6bb537eec58e4234e75e075f96fad89e145
TheTBO <almujtaba.mhmd@gmail.com>
2023-11-11 11:48:55 +0800

    Add and link main.ts

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+const form = document.getElementById("newsletter-form")! as HTMLFormElement;
+const email = form.getElementById("email")! as HTMLInputElement;
+const sucessEmail = document.getElementById(
+  "success-msg-email",
+)! as HTMLSpanElement;
